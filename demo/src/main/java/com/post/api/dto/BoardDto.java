4676387cd4dc93139fbe147207ea718b8e032e82
201411146 <gksdnf050@naver.com>
2020-08-06 02:39:16 +0900

    asd

---
+++ demo/src/main/java/com/post/api/dto/BoardDto.java
@@ -0,0 +1,19 @@
+package com.post.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@ToString
+public class BoardDto {
+    private int id;
+    private String title;
+    private String content;
+    private int categoryId;
+    private Date createDate;
+    private Date modifyDate;
+}
