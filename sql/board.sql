200210d382fd31cc085a8b5675bc1a8cfb741697
rlfalsgh95 <rlfalsgh95@naver.com>
2020-08-17 23:48:15 +0900

    게시글 추가 sql문 추가

--- sql/board.sql
+++ sql/board.sql
@@ -22,5 +22,26 @@ INSERT INTO board VALUES(1, "basic title1", "basic content1", 1, now(), now());
 INSERT INTO board VALUES(2, "basic title2", "basic content2", 2, now(), now());
 INSERT INTO board VALUES(3, "basic title3", "basic content3", 3, now(), now());
 INSERT INTO board VALUES(4, "basic title4", "basic content4", 1, now(), now());
+INSERT INTO board VALUES(5, "basic title5", "basic content1", 1, now(), now());
+
+INSERT INTO board VALUES(6, "test title1", "basic content2", 2, now(), now());
+INSERT INTO board VALUES(7, "test title2", "basic content3", 3, now(), now());
+INSERT INTO board VALUES(8, "test title3", "basic content4", 1, now(), now());
+INSERT INTO board VALUES(9, "test title4", "basic content1", 1, now(), now());
+INSERT INTO board VALUES(10, "test title5", "basic content2", 2, now(), now());
+
+INSERT INTO board VALUES(11, "basic title3", "test content1", 3, now(), now());
+INSERT INTO board VALUES(12, "basic title4", "test content2", 1, now(), now());
+INSERT INTO board VALUES(13, "basic title1", "test content3", 1, now(), now());
+INSERT INTO board VALUES(14, "basic title2", "test content4", 2, now(), now());
+INSERT INTO board VALUES(15, "basic title3", "test content5", 3, now(), now());
+
+INSERT INTO board VALUES(16, "basic title4", "basic content4", 1, now(), now());
+INSERT INTO board VALUES(17, "basic title1", "basic content1", 1, now(), now());
+INSERT INTO board VALUES(18, "basic title2", "basic content2", 2, now(), now());
+INSERT INTO board VALUES(19, "basic title3", "basic content3", 3, now(), now());
+INSERT INTO board VALUES(20, "basic title4", "basic content4", 1, now(), now());
+
+
 
 
